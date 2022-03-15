e4eb1cb9153d31c3e50eb6ae1655c613c1a19c18
GH-Li <liguohao010701@qq.com>
2022-03-15 20:53:55 +0800

    完成时钟功能

--- src/components/config/Item.tsx
+++ src/components/config/Item.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import {StyleSheet, Text, View} from "react-native";
-import {Colors} from "react-native/Libraries/NewAppScreen";
 
 const ConfigItem: React.FC<{
 	label: string;
@@ -22,13 +21,11 @@ const configItemStyles = StyleSheet.create({
 		margin: 5,
 		height: 30,
 		flex: 1,
-		backgroundColor: Colors.darker
 	},
 	itemText: {
 		fontSize: 18,
 		fontWeight: '400',
 		flex:7,
-		textDecorationColor: Colors.lighter
 	},
 	itemChildren: {
 		flex: 3
--- src/views/Clock.tsx
+++ src/views/Clock.tsx
@@ -1,17 +1,46 @@
-import {StyleSheet, View} from "react-native";
+import {StyleSheet, Text, View} from "react-native";
+import dayjs from "dayjs";
+import React, {useEffect, useState} from "react";
 
 
-const Clock = () => {
+const Clock:React.FC<{
+	format: string
+}> = ({children, format}) => {
+	const getTime = (format: string) => {
+		return dayjs().format(format);
+	};
+	let [time, setTime] = useState(getTime(format));
+	const runTime = () => {
+		let tmp = getTime(format);
+		if(tmp !== time){
+			setTime(tmp);
+		}
+	};
+	
+	useEffect(() => {
+		let timer = setInterval(runTime, 400);
+		return () => {
+			clearInterval(timer);
+		}
+	});
+	
 	return (
 		<View style={clockStyles.clockContainer}>
-		
+			<Text style={clockStyles.clockText}>{time}</Text>
+			{children}
 		</View>
 	)
 };
 
 const clockStyles = StyleSheet.create({
 	clockContainer: {
-		height: 100
+		justifyContent: 'center',
+		flex: 1
+	},
+	clockText: {
+		fontSize: 85,
+		fontWeight: "600",
+		textAlign: 'center',
 	}
 })
 
